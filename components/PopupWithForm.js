c27d3c74c801db6aeda92fd9f596be345f836114
brandonquinonez <brandonvquinonez23@gmail.com>
2026-01-16 01:46:27 -0800

    addressed code reviewer feedback

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -4,17 +4,20 @@ class PopupWithForm extends Popup {
   constructor({ popupSelector, handleFormSubmit }) {
     super({ popupSelector });
     this._popupForm = this._popupElement.querySelector(".popup__form");
+    this._inputList = this._popupForm.querySelectorAll(".popup__input");
     this._handleFormSubmit = handleFormSubmit;
   }
 
   _getInputValues() {
-    this._inputList = this._popupForm.querySelectorAll(".popup__input");
-
     const inputValues = {};
     this._inputList.forEach((input) => {
       inputValues[input.name] = input.value; //Added a key/value pair for each input. The key is input.name and the value is input.value. Need to use brackets notation, not dot notation.
     });
     return inputValues;
+  }
+
+  getForm() {
+    return this._popupForm;
   }
 
   setEventListeners() {
--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,6 @@ import PopupWithForm from "../components/PopupWithForm.js";
 import TodoCounter from "../components/TodoCounter.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
-const addTodoPopupEl = document.querySelector("#add-todo-popup");
-const addTodoForm = addTodoPopupEl.querySelector(".popup__form");
-// const todoTemplate = document.querySelector("#todo-template"); --> This was removed but kept here just for reference. It was selected through the getView method in the todo.js file instead.
-
 const todoCounter = new TodoCounter(initialTodos, ".counter__text"); // Here I instantiated the TodoCounter class, passing in the initialTodos array and the selector for the counter text element. REMEMBER that the .counter__text should match the one used in the HTML file
 
 function handleCheck(completed) {
@@ -26,28 +22,27 @@ function handleDelete(completed) {
   todoCounter.updateTotal(false); // Always decrement total count when a todo is deleted
 }
 
+const renderTodo = (item) => {
+  const todo = generateTodo(item);
+  section.addItem(todo);
+};
+
 const addTodoPopup = new PopupWithForm({
   popupSelector: "#add-todo-popup",
   handleFormSubmit: (inputValues) => {
-    // Generate a unique id
-    const id = uuidv4();
+    // Added id directly to inputValues
+    inputValues.id = uuidv4();
 
-    // Format the date and adjust for timezone
+    // Formatted the date and adjust for timezone
     const date = new Date(inputValues.date);
     date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+    inputValues.date = date;
 
-    // Create the values object with all required fields
-    const values = {
-      id,
-      name: inputValues.name,
-      date,
-      completed: false, // New todos are not completed by default
-    };
+    // Set completed to false
+    inputValues.completed = false;
 
-    //Move code from existing submission handler to here (below)
-    // Passed the values object to generateTodo
-    const todo = generateTodo(values);
-    section.addItem(todo);
+    //Generate and add the todo:
+    renderTodo(inputValues);
     todoCounter.updateTotal(true); // <-- Added this line to update total count when a new todo is added
     addTodoPopup.close();
     addTodoForm.reset();
@@ -55,6 +50,9 @@ const addTodoPopup = new PopupWithForm({
   },
 });
 addTodoPopup.setEventListeners();
+
+// Here you get the form from the popup instance:
+const addTodoForm = addTodoPopup.getForm();
 
 // The logic in this function should all be handled in the Todo class.
 //****IMPORTANT NOTE****: Remember that when working outside a class, you call the method by referencing the name of the instance (ex. todo) and you would call the method, ex. --> todo.getView(). Inside the class, for example when you call it, you reference the this._ object by writing this._ first then the name of the method your calling.
@@ -68,30 +66,11 @@ const generateTodo = (data) => {
 //Below is what called instantiation, in this case its done on the Section class
 const section = new Section({
   items: initialTodos, //passed initial todos as an array to items key
-  renderer: (item) => {
-    const todo = generateTodo(item); //generate todo item
-    section.addItem(todo); // Used addItem method from Section class to add the new todo
-  },
+  renderer: renderTodo,
   containerSelector: ".todos__list",
 });
 
 section.renderItems(); //called section instance's renderItems method
-
-// const openModal = (modal) => {
-//   modal.classList.add("popup_visible");
-// };
-
-const closeModal = (modal) => {
-  modal.classList.remove("popup_visible");
-};
-
-//Find the currently opened modal and close it below:
-function handleEscapeClose(evt) {
-  if (evt.key === "Escape") {
-    const openPopup = document.querySelector(".popup_visible");
-    closeModal(openPopup);
-  }
-}
 
 addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
